f5deeb96a7d8ce1123f797bcbff3f390a2c2fb25
jtagrgh <jakob.koblinsky@gmail.com>
2025-12-08 10:13:50 +0000

    day8

---
+++ day8.py
@@ -0,0 +1,92 @@
+from itertools import combinations
+from math import sqrt, prod
+
+
+with open('day8.in', 'r') as file:
+    lines = [x.strip() for x in file.readlines()]
+
+nodes = [tuple(map(int, line.split(','))) for line in lines]
+
+dist = lambda a,b: sqrt(sum((x-y)**2 for x,y in zip(a,b)))
+
+
+def part1():
+    node_combinations = sorted([(dist(a,b), a, b) for a,b in combinations(nodes, 2)], reverse=True)
+
+    graph = {node:[] for node in nodes}
+
+    for _ in range(1000):
+        _,a,b = node_combinations.pop()
+        graph[a].append(b)
+        graph[b].append(a)
+
+    visited = set()
+
+    def ffill(node):
+        if node in visited:
+            return 0
+        
+        node_count = 0
+        stack = [node]
+        while stack:
+            vis_node = stack.pop()
+            if vis_node in visited:
+                continue
+            visited.add(vis_node)
+            node_count += 1
+            stack.extend(graph[vis_node])
+
+        return node_count
+
+    node_counts = []
+
+    for node in graph.keys():
+        node_counts.append(ffill(node))
+
+    node_counts.sort()
+
+    print(prod(node_counts[-3:]))
+
+
+def part2():
+    node_combinations = sorted([(dist(a,b), a, b) for a,b in combinations(nodes, 2)], reverse=True)
+
+    graph = {node:[] for node in nodes}
+
+    def gt_one_subgraph():
+        visited = set()
+
+        subgraph_count = 0
+
+        for node in graph.keys():
+            node_count = 0
+            stack = [node]
+
+            while stack:
+                vis_node = stack.pop()
+                if vis_node in visited:
+                    continue
+                visited.add(vis_node)
+                node_count += 1
+                stack.extend(graph[vis_node])
+
+            if node_count > 0:
+                subgraph_count += 1
+
+            if subgraph_count > 1:
+                return True
+        
+        return False
+    
+    a = b = (0,0,0)
+
+    while gt_one_subgraph():
+        _,a,b = node_combinations.pop()
+        graph[a].append(b)
+        graph[b].append(a)
+
+    print(a[0] * b[0])
+
+
+part1()
+part2()
